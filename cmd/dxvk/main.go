4baa27ab7d4d7ba7f502843953b98fba62544e7b
sewn <sewn@disroot.org>
2025-10-10 14:29:20 +0300

    refactor(dxvk)!: implement DLL version, cmd, pfx env append

---
+++ cmd/dxvk/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/sewnie/wine"
+	"github.com/sewnie/wine/dxvk"
+)
+
+func main() {
+	var version string
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "usage: %s [flags] install|uninstall\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+	flag.StringVar(&version, "ver", "2.7.1", "dxvk version to install")
+	flag.Parse()
+
+	pfx := wine.New(os.Getenv("WINEPREFIX"), "")
+	pfx.Env = append(pfx.Env, "WINEDLLOVERRIDES=winemenubuilder.exe=")
+	if !pfx.Exists() {
+		log.Println("Initializing Wineprefix")
+
+		err := pfx.Init().Run()
+		if err != nil {
+			log.Fatalln("failed to initialize:", err)
+		}
+	}
+
+	v, err := dxvk.Version(pfx)
+	if err != nil {
+		log.Fatal("failed to find installed dxvk:", err)
+	}
+	log.Println("Installed DXVK version:", v)
+
+	switch flag.Arg(0) {
+	case "install":
+		err := installDXVK(pfx, version)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "uninstall":
+		err := dxvk.Restore(pfx)
+		if err != nil {
+			log.Fatal(err)
+		}
+	case "env":
+		log.Println(pfx.Env)
+		dxvk.EnvOverride(pfx, true)
+		log.Println(pfx.Env)
+	default:
+		flag.Usage()
+		os.Exit(1)
+	}
+}
+
+func installDXVK(pfx *wine.Prefix, version string) error {
+	out, err := os.CreateTemp("", "dxvk.*.tar.gz")
+	if err != nil {
+		return fmt.Errorf("tempfile: %w", err)
+	}
+	defer os.Remove(out.Name())
+
+	resp, err := http.Get(dxvk.URL(version))
+	if err != nil {
+		return fmt.Errorf("get: %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return fmt.Errorf("download: %w", err)
+	}
+
+	return dxvk.Extract(pfx, out)
+}
--- dxvk/dxvk.go
+++ dxvk/dxvk.go
@@ -1,49 +1,60 @@
-// Package dxvk implements routines to install DXVK
+// Package dxvk manages DXVK for a Wineprefix.
+//
+// To make DXVK usable by Wine applications, it is reccomended to use either
+// [Variable] for runtime setting or [AddOverrides] for a more permanent setting.
 package dxvk
 
 import (
 	"archive/tar"
+	"bytes"
 	"compress/gzip"
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
+	"strings"
 
 	"github.com/sewnie/wine"
+	"github.com/sewnie/wine/peutil"
 )
 
 const Repo = "https://github.com/doitsujin/dxvk"
 
-// To make DXVK usable by Wine applications, it is reccomended to use either
-// [Variable] for runtime setting or [AddOverrides] for a more permanent setting.
-//
-// In the DLL overrides, setting the value to native,builtin allows Wine
-// to use DXVK if available. If the DVXK DLLs are installed in the prefix,
-// and DXVK is not to be used, then the value should be "builtin" instead;
-// this is only supported in the environment variable application.
-
-// Variable returns a Wine DLL overrides value,
-// with whether to use DXVK DLLs or not. This should be appended
-// to a final WINEDLLOVERRIDES value.
-func EnvOverride(enabled bool) string {
-	overrides := "d3d9,d3d10core,d3d11,dxgi="
+// EnvOverride appends DXVK DLL overrides to the given Wineprefix's
+// environment variables.
+func EnvOverride(pfx *wine.Prefix, enabled bool) {
+	name := "WINEDLLOVERRIDES"
+	val := "d3d9,d3d10core,d3d11,dxgi="
 	if enabled {
-		overrides += "native"
+		val += "native"
 	} else {
-		overrides += "builtin"
+		val += "builtin"
 	}
-	return overrides
+
+	for i, env := range pfx.Env {
+		if !strings.HasPrefix(env, name) {
+			continue
+		}
+
+		pfx.Env[i] += ";" + val
+		return
+	}
+
+	pfx.Env = append(pfx.Env, name+"="+val)
 }
 
-// Restore removes the DXVK overridden DLLs from the given wineprefix, then
-// restores Wine DLLs.
+// Restore restores Direct3D DLLs, which were overwritten by DXVK, in the wineprefix.
 func Restore(pfx *wine.Prefix) error {
-	for _, dir := range []string{"syswow64", "system32"} {
-		for _, dll := range []string{"d3d9", "d3d10core", "d3d11", "dxgi"} {
-			p := filepath.Join(pfx.Dir(), "drive_c", "windows", dir, dll+".dll")
+	dirs := []string{"syswow64", "system32"}
+	names := []string{"d3d8", "d3d9", "d3d10core", "d3d11", "dxgi"}
 
-			if err := os.Remove(p); err != nil {
+	for _, dir := range dirs {
+		for _, name := range names {
+			dll := filepath.Join(pfx.Dir(), "drive_c", "windows", dir, name+".dll")
+
+			if err := os.Remove(dll); err != nil {
 				if errors.Is(err, os.ErrNotExist) {
 					continue
 				}
@@ -60,16 +71,59 @@ func URL(ver string) string {
 	return fmt.Sprintf("%s/releases/download/v%[2]s/dxvk-%[2]s.tar.gz", Repo, ver)
 }
 
-// Extract extracts DXVK's DLLs into the given wineprefix -
-// overriding Wine's D3D DLLs, given the path to a valid DXVK tarball.
-func Extract(pfx *wine.Prefix, name string) error {
-	tf, err := os.Open(name)
+// Version returns the DXVK version of the system32 d3d11 DLL installed
+// in the wineprefix. The 'd3d11' DLL is chosen as it is one of
+// the only DXVK DLLs that contain versioning.
+//
+// If other DLLs such as d3d8 are needed to track, it is reccomended
+// to store the installed version of DXVK prior to [Extract].
+func Version(pfx *wine.Prefix) (string, error) {
+	return dllVersion(filepath.Join(
+		pfx.Dir(), "drive_c", "windows", "system32", "d3d11.dll"))
+}
+
+// Only valid for d3d9, d3d11 & dxgi
+func dllVersion(name string) (string, error) {
+	f, err := peutil.Open(name)
 	if err != nil {
+		return "", err
+	}
+	defer f.Close()
+
+	for _, s := range f.Sections {
+		if s.Name != ".rdata" {
+			continue
+		}
+		b, err := s.Data()
+		if err != nil {
+			log.Fatal(err)
+		}
+		head := []byte("DXVK: \x00v")
+		start := bytes.Index(b, head)
+		if start < 0 {
+			break
+		}
+		verStart := start + len(head)
+		verEnd := bytes.IndexByte(b[verStart:], 0)
+		if verEnd < 0 {
+			break
+		}
+		return string(b[verStart : verStart+verEnd]), nil
+	}
+
+	return "", nil
+}
+
+// Extract installs the DXVK DLLs by seeking to the start of
+// tarball and extracting the gzipped contents onto the given
+// wineprefix. Extract will override Wine DLLs; to use it,
+// you will have to add DLL overrides via [EnvOverride].
+func Extract(pfx *wine.Prefix, tarball io.ReadSeeker) error {
+	if _, err := tarball.Seek(0, io.SeekStart); err != nil {
 		return err
 	}
-	defer tf.Close()
 
-	zr, err := gzip.NewReader(tf)
+	zr, err := gzip.NewReader(tarball)
 	if err != nil {
 		return err
 	}
@@ -114,6 +168,8 @@ func Extract(pfx *wine.Prefix, name string) error {
 		if err != nil {
 			return err
 		}
+
+		log.Println("dxvk: Installing", dst)
 
 		if _, err = io.Copy(f, tr); err != nil {
 			f.Close()
